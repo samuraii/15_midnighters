ed8c1389b71842139dd2a94767fca4fe78e82462
samuraii <7thsamurai@mail.ru>
2018-04-07 18:38:47 +0300

    Правки по ревью
    1) Сделал получение информациии через yield 2) Изменил работу с timezone, передал напрямую в fromtimestamp 3) Изменил хардкод количества страниц на получение из ответа сервера

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -3,14 +3,18 @@ from pytz import timezone
 from datetime import datetime
 
 
-def load_attempts_data(pages):
+def load_attempts_data():
     url = 'http://devman.org/api/challenges/solution_attempts/'
-    attempts = []
-    for page in range(1, pages + 1):
+    page = 1
+    while True:
         page_param = {'page': page}
-        attempts_data = requests.get(url, params=page_param).json()['records']
-        attempts += attempts_data
-    return attempts
+        attempts_data = requests.get(url, params=page_param).json()
+        number_of_pages = attempts_data['number_of_pages']
+        for attempt in attempts_data['records']:
+            yield attempt
+        page += 1
+        if page > number_of_pages:
+            break
 
 
 def get_midnighters(attempts_data):
@@ -18,9 +22,11 @@ def get_midnighters(attempts_data):
     midnight = 0
     morning = 6
     for attempt in attempts_data:
-        user_timezone = attempt['timezone']
-        attempt_time_utc = datetime.fromtimestamp(attempt['timestamp'])
-        attempt_time_local = timezone(user_timezone).fromutc(attempt_time_utc)
+        user_timezone = timezone(attempt['timezone'])
+        attempt_time_local = datetime.fromtimestamp(
+            attempt['timestamp'],
+            tz=user_timezone
+        )
         if midnight < attempt_time_local.hour < morning:
             owl_users.append(attempt['username'])
     return set(owl_users)
@@ -33,7 +39,5 @@ def print_owl_users(user_list):
 
 
 if __name__ == '__main__':
-    available_pages = 10
-    attempts_data = load_attempts_data(available_pages)
-    owl_users = get_midnighters(attempts_data)
+    owl_users = get_midnighters(load_attempts_data())
     print_owl_users(owl_users)
