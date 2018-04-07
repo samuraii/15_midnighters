dc60a80fe4db0a49e55fc8a4ec0f7ab363a501a3
samuraii <7thsamurai@mail.ru>
2018-04-07 15:40:27 +0300

    Решил задание
    Решение задания "Совы"

--- README.md
+++ README.md
@@ -1,7 +1,46 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+Script allows to list users who made attempts to pass the task from midnight (00:00) to (06:00) in the morning.
 
+# How to use
+
+In order to run the script the additional libraries must be installed:
+
+```bash
+pip install -r requiremets.txt
+```
+
+To run script:
+
+```bash
+python python seek_dev_nighters.py
+```
+
+Output:
+
+```bash
+Made attempts between 00:00 and 06:00:
+cashncarry
+id311542249
+dreamfall3r
+khokhlov86
+kachalov85
+maniak.tux
+id441633628
+id106957374
+0lekciy
+leonid.soputnyak
+maxdmtrev
+andreyoleyn
+oeananas
+SergeyKhimchenko
+AlexanderAkopov
+id306803365
+IvanKumeyko
+apelsinovv
+paganismrus
+```
+ 
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,38 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import requests
+from pytz import timezone
+from datetime import datetime
 
-def get_midnighters():
-    pass
+
+def load_attempts_data(pages):
+    url = 'http://devman.org/api/challenges/solution_attempts/'
+    attempts = []
+    for page in range(1, pages + 1):
+        page_param = {'page': page}
+        attempts_data = requests.get(url, params=page_param).json()['records']
+        attempts += attempts_data
+    return attempts
+
+
+def get_midnighters(attempts_data):
+    owl_users = []
+    midnight = 0
+    morning = 6
+    for attempt in attempts_data:
+        user_timezone = attempt['timezone']
+        attempt_time_utc = datetime.fromtimestamp(attempt['timestamp'])
+        attempt_time_local = timezone(user_timezone).fromutc(attempt_time_utc)
+        if midnight < attempt_time_local.hour < morning:
+            owl_users.append(attempt['username'])
+    return set(owl_users)
+
+
+def print_owl_users(user_list):
+    print('Made attempts between 00:00 and 06:00:')
+    for user in user_list:
+        print(user)
+
 
 if __name__ == '__main__':
-  pass
+    attempts_data = load_attempts_data(10)
+    owl_users = get_midnighters(attempts_data)
+    print_owl_users(owl_users)
